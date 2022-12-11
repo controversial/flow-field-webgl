dafa9097ab213d44d3a0a7c72489324a50b4d8c8
Luke Taylor <luke@deentaylor.com>
2022-12-11 14:07:57 -0500

    Invert texture coordinates

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -8,10 +8,12 @@ uniform float u_screen_dpr;
 out vec4 color;
 
 void main() {
-  vec2 st = gl_FragCoord.xy / u_resolution;
+  vec2 coord = vec2(gl_FragCoord.x, u_resolution.y - gl_FragCoord.y);
+  vec2 st = coord / u_resolution;
+
   float checkerboard_size = 20.0 * u_screen_dpr;
-  int stripes_x = int(gl_FragCoord.x / checkerboard_size) % 2;
-  int stripes_y = int(gl_FragCoord.y / checkerboard_size) % 2;
+  int stripes_x = int(coord.x / checkerboard_size) % 2;
+  int stripes_y = int(coord.y / checkerboard_size) % 2;
   int checkerboard = (stripes_x + stripes_y) % 2;
 
   color = vec4(vec3(checkerboard), 1);
