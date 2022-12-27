b5f5e80bc1eda63df4913645d5c010a983ce82dc
Luke Taylor <luke@deentaylor.com>
2022-12-27 17:55:03 -0500

    new lines coloring

--- src/shaders/lines-frag.glsl
+++ src/shaders/lines-frag.glsl
@@ -18,12 +18,6 @@ flat in int v_line_index;
 out vec4 color;
 
 
-// TODO: replace with non-random coloring
-float PHI = 1.61803398874989484820459;  // Φ = Golden Ratio
-float gold_noise(in vec2 xy, in float seed){
-  return fract(tan(distance(xy*PHI, xy)*seed)*xy.x);
-}
-
 void main() {
   // Antialias the left and right edges of the line
   float tx = v_uv.x;
@@ -32,13 +26,9 @@ void main() {
   float edge_distance_px = edge_distance * adjusted_line_width;
   float feather = smoothstep(0., u_line_feather_width, edge_distance_px);
 
-  color = vec4(
-    gold_noise(vec2(37., 1.), float(v_line_index)),
-    gold_noise(vec2(37., 2.), float(v_line_index)),
-    gold_noise(vec2(37., 3.), float(v_line_index)),
-    feather
-  );
-  color.rgb *= color.a;
+  float base_alpha = u_line_alpha;
+  float gradient = v_uv.y;
 
-  // color = vec4(1., 1., 1., base_alpha * gradient * feather);
+  color = vec4(1., 1., 1., base_alpha * gradient * feather);
+  color.rgb *= color.a;
 }
