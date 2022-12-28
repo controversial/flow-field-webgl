351fd89adc4dae3eb1e7899433efd4b530a3edf1
Luke Taylor <luke@deentaylor.com>
2022-12-28 00:23:18 -0500

    Add Program class to streamline uniform bindings

--- src/utils/shader.ts
+++ src/utils/shader.ts
@@ -32,3 +32,129 @@ export function createProgram(gl: WebGL2RenderingContext, vertexShaderSrc: strin
   if (!program) throw new Error('Failed to create program');
   return program;
 }
+
+
+
+export type GlslTypeValuePair =
+  | { type: 'float', value: number }
+  | { type: 'int', value: number }
+  | { type: 'uint', value: number }
+  | { type: 'vec2', value: [number, number] }
+  | { type: 'vec3', value: [number, number, number] }
+  | { type: 'vec4', value: [number, number, number, number] }
+  | { type: 'ivec2', value: [number, number] }
+  | { type: 'ivec3', value: [number, number, number] }
+  | { type: 'ivec4', value: [number, number, number, number] }
+  | { type: 'uvec2', value: [number, number] }
+  | { type: 'uvec3', value: [number, number, number] }
+  | { type: 'uvec4', value: [number, number, number, number] }
+  | { type: 'sampler2D', value: WebGLTexture }
+  | { type: 'isampler2D', value: WebGLTexture }
+  | { type: 'usampler2D', value: WebGLTexture };
+export type GlslType = GlslTypeValuePair['type'];
+export type GlslTypeMaybeValue = GlslTypeValuePair | Omit<GlslTypeValuePair, 'value'>
+
+export type LooseUniformsDefinition = Record<string, GlslTypeMaybeValue>;
+export type UniformsDefinition = Record<string, GlslTypeValuePair>;
+
+type UniformRecord = GlslTypeMaybeValue & {
+  name: `u_${string}`;
+  location: ReturnType<WebGL2RenderingContext['getUniformLocation']>;
+}
+type CompleteUniformRecord = GlslTypeValuePair & {
+  name: `u_${string}`;
+  location: ReturnType<WebGL2RenderingContext['getUniformLocation']>;
+}
+
+interface AttributeRecord {
+  name: `a_${string}`;
+  location: ReturnType<WebGL2RenderingContext['getAttribLocation']>;
+}
+
+const camelToSnake = (str: string) => str.replace(/[A-Z]/g, (letter) => `_${letter.toLowerCase()}`);
+const getUniformName = (str: string) => `u_${str.includes('_') ? str : camelToSnake(str)}` as const;
+const getAttributeName = (str: string) => `a_${str.includes('_') ? str : camelToSnake(str)}` as const;
+
+function assertNever(x: never): never {
+  throw new Error('Unexpected object: ' + x);
+}
+
+
+export class Program {
+  gl: WebGL2RenderingContext;
+  vertexShaderSrc: string;
+  fragmentShaderSrc: string;
+  program: WebGLProgram;
+  attributes: Record<string, AttributeRecord>;
+  uniforms: Record<string, UniformRecord>;
+
+  constructor(
+    gl: WebGL2RenderingContext,
+    vertexShaderSrc: string,
+    fragmentShaderSrc: string,
+    attributeNames: string[] = [],
+    uniforms: LooseUniformsDefinition = {},
+  ) {
+    this.gl = gl;
+    this.vertexShaderSrc = vertexShaderSrc;
+    this.fragmentShaderSrc = fragmentShaderSrc;
+    this.program = createProgram(this.gl, vertexShaderSrc, fragmentShaderSrc);
+
+    this.attributes = Object.fromEntries(attributeNames.map((name) => {
+      const transformedName = getAttributeName(name);
+      const location = this.gl.getAttribLocation(this.program, transformedName);
+      return [name, { name: transformedName, location }];
+    }));
+    this.uniforms = Object.fromEntries(Object.entries(uniforms).map(([name, definition]) => {
+      const transformedName = getUniformName(name);
+      const location = this.gl.getUniformLocation(this.program, transformedName);
+      return [name, { ...definition, name: transformedName, location }];
+    }));
+    console.log(this.uniforms);
+    console.log(this.attributes);
+  }
+
+  get uniformsList() { return Object.values(this.uniforms); }
+  get attributesList() { return Object.values(this.attributes); }
+
+  use(overrides: UniformsDefinition = {}) {
+    this.gl.useProgram(this.program);
+    if (Object.keys(overrides).length) this.bindUniforms(overrides);
+  }
+
+  bindUniforms(overrides: UniformsDefinition) {
+    const overrides2 = Object.entries(overrides).map(([name, definition]) => ({ ...definition, name: getUniformName(name) }));
+    const uniforms = this.uniformsList.map((uniform) => ({
+      ...uniform,
+      ...[...overrides2].reverse().find((override) => {
+        if (override.name !== uniform.name) return false;
+        if (override.type !== uniform.type) throw new Error(`Uniform ${override.name} has type ${override.type} but was expected to be ${uniform.type}`);
+        return true;
+      }),
+    })) as UniformRecord[];
+    const isComplete = (uniform: UniformRecord): uniform is CompleteUniformRecord => 'value' in uniform;
+    const completeUniforms = uniforms.filter(isComplete);
+
+    let textureCount = 0;
+    completeUniforms.forEach((uniform) => {
+      if (uniform.type === 'sampler2D' || uniform.type === 'isampler2D' || uniform.type === 'usampler2D') {
+        this.gl.uniform1i(uniform.location, textureCount);
+        this.gl.activeTexture(this.gl.TEXTURE0 + textureCount);
+        this.gl.bindTexture(this.gl.TEXTURE_2D, uniform.value);
+        textureCount++;
+      } else if (uniform.type === 'float') {
+        this.gl.uniform1f(uniform.location, uniform.value);
+      } else if (uniform.type === 'int' || uniform.type === 'uint') {
+        this.gl.uniform1i(uniform.location, uniform.value);
+      } else if (uniform.type === 'vec2' || uniform.type === 'vec3' || uniform.type === 'vec4') {
+        const name = `uniform${uniform.value.length}fv` as const;
+        this.gl[name](uniform.location, uniform.value);
+      } else if (uniform.type === 'ivec2' || uniform.type === 'ivec3' || uniform.type === 'ivec4' || uniform.type === 'uvec2' || uniform.type === 'uvec3' || uniform.type === 'uvec4') {
+        const name = `uniform${uniform.value.length}iv` as const;
+        this.gl[name](uniform.location, uniform.value);
+      } else {
+        assertNever(uniform); // compile error if we didn’t handle all cases :)
+      }
+    });
+  }
+}
