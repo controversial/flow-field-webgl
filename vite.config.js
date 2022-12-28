afe7ddaf190880538e21181a638e6b7e98ea9e59
Luke Taylor <luke@deentaylor.com>
2022-12-28 16:09:58 -0500

    increase performance.now resolution

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,11 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [glsl()],
+  server: {
+    headers: {
+      // Enable high-resolution timers for performance measuring (see https://web.dev/coop-coep/)
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+      'Cross-Origin-Opener-Policy': 'same-origin',
+    },
+  },
 });
