800efee5a18614fd1f8ca839b2c0495b3fa23834
Luke Taylor <luke@deentaylor.com>
2022-12-29 02:52:13 -0500

    production bundle optimizaiton

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,27 @@
 import glsl from 'vite-plugin-glsl';
 import { defineConfig } from 'vite';
 
+
 export default defineConfig({
-  plugins: [glsl()],
+  plugins: [glsl({ compress: true })],
+
+  // Enable high-resolution timers for performance measuring (see https://web.dev/coop-coep/)
   server: {
     headers: {
-      // Enable high-resolution timers for performance measuring (see https://web.dev/coop-coep/)
       'Cross-Origin-Embedder-Policy': 'require-corp',
       'Cross-Origin-Opener-Policy': 'same-origin',
+    },
+  },
+
+  // Split tweakpane into its own chunk
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (/node_modules\/@?tweakpane/.test(id)) return 'tweakpane';
+          else if (/node_modules\//.test(id)) return 'vendor';
+        },
+      },
     },
   },
 });
