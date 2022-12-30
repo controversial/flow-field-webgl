a90338ffec7e44d5cf96295a58aef0120b895883
Luke Taylor <luke@deentaylor.com>
2022-12-30 12:53:22 -0500

    fix global typings

---
+++ src/ambient.d.ts
@@ -0,0 +1,28 @@
+/// <reference types="vite/client" />
+
+declare module '*.glsl' {
+  const value: string;
+  export default value;
+}
+
+declare module 'seedrandom/lib/alea' {
+  const alea: import('seedrandom')['alea'];
+  export default alea;
+}
+
+interface Performance {
+  measureUserAgentSpecificMemory?(): Promise<{
+    bytes: number,
+    breakdown: ({
+      types: string[],
+      bytes: number,
+    })[],
+  }>;
+}
+
+interface ObjectConstructor {
+  // redefine Object.fromEntries to preserve key type as long as that key type extends string
+  // I think this produces correct results in all cases
+  fromEntries<K extends string, V>(entries: Iterable<readonly [K, V]>): Record<K, V>;
+  // 2 overloads from typescript core are preserved
+}
--- src/global.d.ts
+++ src/global.d.ts
@@ -1,34 +1,5 @@
-/// <reference types="vite/client" />
-
 import type LineField from './passes/line-field';
 import type Renderer from './renderer';
-
-declare module '*.glsl' {
-  const value: string;
-  export default value;
-}
-
-declare module 'seedrandom/lib/alea' {
-  const alea: import('seedrandom')['alea'];
-  export default alea;
-}
-
-interface Performance {
-  measureUserAgentSpecificMemory?(): Promise<{
-    bytes: number,
-    breakdown: ({
-      types: string[],
-      bytes: number,
-    })[],
-  }>;
-}
-
-interface ObjectConstructor {
-  // redefine Object.fromEntries to preserve key type as long as that key type extends string
-  // I think this produces correct results in all cases
-  fromEntries<K extends string, V>(entries: Iterable<readonly [K, V]>): Record<K, V>;
-  // 2 overloads from typescript core are preserved
-}
 
 declare global {
   interface Window {
