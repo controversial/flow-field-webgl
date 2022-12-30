1026a30dc5e25ada9803b26aa3e14b0b56a9c433
Luke Taylor <luke@deentaylor.com>
2022-12-30 12:37:34 -0500

    tweak overscan calculations

--- src/shaders/lines-vert.glsl
+++ src/shaders/lines-vert.glsl
@@ -26,37 +26,43 @@ void main() {
   int line_index = gl_InstanceID;
   int point_index = a_line_point;
 
-  vec2 point_pos = getPointPosition(line_index, point_index);
+  float orientation = float(a_normal); // this is either a left-side point or a right-side point of the thick line
+  float adjusted_line_width = u_line_width + (u_line_feather_width / 2.) * 2.; // to make line width appear the same after we feather the edges
+  vec2 px = 1. / u_resolution * u_screen_dpr; // normalized pixel size (equivalent across screens)
 
+  // Sample 3 points from the line
+  vec2 point_pos = getPointPosition(line_index, point_index);
   vec2 prev_point_pos = getPointPosition(line_index, point_index - 1);
   vec2 next_point_pos = getPointPosition(line_index, point_index + 1);
   float has_prev = step(0.5, float(point_index)); // point_index < 0 before the first point in the line
   float has_next = step(float(point_index), float(u_num_line_points - 1) - 0.5); // point_index > (u_num_line_points - 1) after the last point in the line
 
+  // move points to adjust for overscan
+  vec2 overscan = (adjusted_line_width * 0.5 + 2.) * px; // how much on each side should we clip off to hide lines that run out of bounds?
+  vec2 overscan_scale = 1. / (1. - overscan * 2.); // How much should we scale positions by to account for overscan?
+  point_pos = ((point_pos * 2. - 1.) * overscan_scale) * 0.5 + 0.5;
+  prev_point_pos = ((prev_point_pos * 2. - 1.) * overscan_scale) * 0.5 + 0.5;
+  next_point_pos = ((next_point_pos * 2. - 1.) * overscan_scale) * 0.5 + 0.5;
+
+  // Compute vector normal to this point of the line using the next/previous points
   vec2 t1 = point_pos - prev_point_pos;              // vector from previous point to current point
   vec2 n1 = normalize(vec2(t1.y, -t1.x));            // perpendicular normal
   vec2 t2 = next_point_pos - point_pos;              // vector from current point to next point
   vec2 n2 = normalize(vec2(t2.y, -t2.x));            // perpendicular normal
   vec2 n = normalize(n1 * has_prev + n2 * has_next); // normal for this point is in between the normals of the adjacent lines
 
-  // there will be two points for this position; we move them in opposite directions along the computed normal `n` to make a thick line
-  float orientation = float(a_normal);
-  float adjusted_line_width = u_line_width + (u_line_feather_width / 2.) * 2.; // to account for feathering the edges
-  vec2 px = 1. / u_resolution * u_screen_dpr; // normalized pixel size (equivalent across screens)
-  vec2 offset = n * orientation * 0.5 * adjusted_line_width * px; // move both points outwards along the normal.
-
-  // how much on each side should we clip off to hide lines that run out of bounds?
-  vec2 overscan = (adjusted_line_width * 0.5 + 2.) * px;
-  // How much should we scale positions by to account for overscan?
-  vec2 overscan_scale = 1. / (1. - overscan * 2.);
-
+  // move this point (which is either the “left” or “right” point) outwards along the normal
+  vec2 offset = n * orientation * 0.5 * adjusted_line_width * px;
   vec2 pos = point_pos + offset;
+
+  // Report final position in clip space
   gl_Position = vec4(
-    (pos * 2. - 1.) * overscan_scale,
+    pos * 2. - 1.,
     0,
     1
   );
 
+  // Report varyings for fragment shader
   v_line_index = line_index;
   v_uv = vec2(
     // How far horizontally across the line does this point fall?
